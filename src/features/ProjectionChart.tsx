dd924af0f3e9777ba2e59fefaba2ddf8bc0a6a00
Ian Brunner <>
2025-04-08 17:56:40 -0500

    basic portfolio value chart

--- src/features/ProjectionChart.tsx
+++ src/features/ProjectionChart.tsx
@@ -1,15 +1,60 @@
-// interface ChartProps {
-//   // Add your props here
-// }
+import usePortfolioCalcuations from '@/hooks/usePortfolioCalcuations';
+import useFormStore from '@/lib/store';
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+// import useBtcPrice from '@/hooks/useBtcPrice';
 
-const Chart = () => {
+const ProjectionChart = () => {
+  const { formData } = useFormStore();
+  const { initialInvestment } = formData;
+
+  const { calculatedPortfolioValue } = usePortfolioCalcuations();
+
+  // const { btcPrice } = useBtcPrice();
+
+  const data = [
+    {
+      name: 'Start',
+      portfolioValue: initialInvestment,
+      // btcValue: 78000,
+    },
+    {
+      name: 'End',
+      portfolioValue: calculatedPortfolioValue?.estimatedValue,
+      // btcValue: 100000,
+    },
+  ];
+
   return (
     <div className="mb-2 h-[300px] w-full md:h-[500px]">
-      <div className="text-secondary flex h-full w-full items-center justify-center bg-gray-200">
-        projection chart placeholder
+      <div className="text-secondary bg-secondary flex h-full w-full items-center justify-center">
+        <ResponsiveContainer width="90%" height="80%">
+          <AreaChart
+            data={data}
+            margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+          >
+            <XAxis dataKey="name" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="portfolioValue"
+              stroke="#8884d8"
+              fill="#8884d8"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
 };
 
-export default Chart;
+export default ProjectionChart;
