3c6a56ed4ae0d5dac635af738237e5927c9f5cba
Ian Brunner <>
2025-04-25 21:09:38 -0500

    basic gradient backgrounds

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -6,26 +6,29 @@ import Summary from '@/features/Summary';
 
 const AppLayout = () => {
   return (
-    <div className="bg-tertiary text-primary h-full min-h-screen">
-      {/* Header */}
-      <header className="bg-secondary sticky top-0 z-10 w-full p-4 text-white shadow-md">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-4">
-            <img src={logo} alt="Calculator Coin" className="h-16 w-16" />
-            <h1 className="text-2xl font-bold">My Coin Calculator</h1>
+    <div className="text-primary relative h-full min-h-screen bg-slate-900">
+      <div className=".calculator-gradient">
+        <div className="bokeh-overlay" />
+        {/* Header */}
+        <header className="header-gradient sticky top-0 z-10 w-full overflow-hidden p-4 text-white shadow-md">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-4">
+              <img src={logo} alt="Calculator Coin" className="h-16 w-16" />
+              <h1 className="text-2xl font-bold">My Coin Calculator</h1>
+            </div>
+            <BitcoinPrice />
           </div>
-          <BitcoinPrice />
-        </div>
-      </header>
-      {/* Main */}
-      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <div className="flex flex-col md:flex-row md:gap-4">
-          <div className="md:w-5/10 mb-8 w-full md:mb-0">
-            <CalculatorForm />
-          </div>
-          <div className="md:w-5/10 w-full">
-            <Chart />
-            <Summary />
+        </header>
+        {/* Main */}
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <div className="flex flex-col md:flex-row md:gap-4">
+            <div className="md:w-5/10 mb-8 w-full md:mb-0">
+              <CalculatorForm />
+            </div>
+            <div className="md:w-5/10 w-full">
+              <Chart />
+              <Summary />
+            </div>
           </div>
         </div>
       </div>
